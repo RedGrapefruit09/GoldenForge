53a573111334ad4e1f12a23224bf292822d74e02
RedGrapefruit <karpovanton729@gmail.com>
2022-03-07 10:24:54 +0300

    1.18.2 port

--- src/main/kotlin/com/redgrapefruit/goldenforge/init/ModFeatures.kt
+++ src/main/kotlin/com/redgrapefruit/goldenforge/init/ModFeatures.kt
@@ -4,29 +4,36 @@ import com.redgrapefruit.goldenforge.util.IInitializer
 import com.redgrapefruit.goldenforge.util.registerConfiguredFeature
 import com.redgrapefruit.goldenforge.util.registerOreBiomeModification
 import com.redgrapefruit.goldenforge.util.registerPlacedFeature
+import net.minecraft.util.registry.RegistryEntry
 import net.minecraft.world.gen.YOffset
-import net.minecraft.world.gen.decorator.CountPlacementModifier
-import net.minecraft.world.gen.decorator.HeightRangePlacementModifier
-import net.minecraft.world.gen.decorator.SquarePlacementModifier
+import net.minecraft.world.gen.feature.ConfiguredFeature
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier
 import net.minecraft.world.gen.feature.Feature
 import net.minecraft.world.gen.feature.OreConfiguredFeatures
 import net.minecraft.world.gen.feature.OreFeatureConfig
+import net.minecraft.world.gen.feature.PlacedFeature
 
 /** A registry for the mod's features, configured features and placed features. */
 object ModFeatures : IInitializer {
     // Configured features
-    val CONFIGURED_STEEL_ORE = Feature.ORE.configure(OreFeatureConfig(
-        listOf(
-            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, ModBlocks.STEEL_ORE.defaultState),
-            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_STEEL_ORE.defaultState)
-        ),
-        7))
+    val CONFIGURED_STEEL_ORE = ConfiguredFeature(
+        Feature.ORE,
+        OreFeatureConfig(
+            listOf(
+                OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, ModBlocks.STEEL_ORE.defaultState),
+                OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_STEEL_ORE.defaultState)
+            ), 7))
 
     // Placed features
-    val PLACED_STEEL_ORE = CONFIGURED_STEEL_ORE.withPlacement(
-        CountPlacementModifier.of(16),
-        SquarePlacementModifier.of(),
-        HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(35)))
+    val PLACED_STEEL_ORE = PlacedFeature(
+        RegistryEntry.of(CONFIGURED_STEEL_ORE),
+        listOf(
+            CountPlacementModifier.of(16),
+            SquarePlacementModifier.of(),
+            HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(35))
+        ))
 
     override fun initialize() {
         registerConfiguredFeature("steel_ore", CONFIGURED_STEEL_ORE)
